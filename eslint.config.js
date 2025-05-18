cfffb73b1a5b44c6e0b157106eaf0123748efd2c
tuaregsand <againdrippy@outlook.fr>
2025-05-19 00:54:08 +0200

    Chore: add eslint config for codex

---
+++ eslint.config.js
@@ -0,0 +1,11 @@
+import js from '@eslint/js';
+import prettier from 'eslint-config-prettier';
+
+export default [
+  js.configs.recommended,
+  {
+    languageOptions: { parserOptions: { ecmaVersion: 2022, sourceType: 'module' } },
+    ignores: ['dist/**', 'out/**', '*.config.*'],
+  },
+  prettier,
+];
